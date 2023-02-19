95657a2a0ec975cbd3cd3179e15f0ce494dbcf95
Abhishek Singh <aks3555@gmail.com>
2023-02-19 12:48:36 +0530

    added comments for explaination

--- javascript/03/oop-in-js/inheritence.js
+++ javascript/03/oop-in-js/inheritence.js
@@ -7,14 +7,31 @@ obj2.b = 20
 let obj3 = Object.create(obj2)
 obj3.c = 30
 
-console.log(obj1)
-console.log(obj2)
-console.log(obj3)
-console.log(obj1.a)
-console.log(obj3.a)
+console.log(obj1) // {a:10}
+console.log(obj2) // {b:20}
+console.log(obj3) // {c:30}
+console.log(obj1.a) // 10
+console.log(obj3.a) // 10 -> how? because obj3 doesn't have a in the console log statement above.
+
 obj3.a = 31
-console.log(obj3.a)
-console.log(obj3)
+// will this also change obj1.a?
+// No.
+
+console.log(obj3.a) // 31
+
+console.log(obj3) // {a:31, c: 30}
+// Note that now a is part of obj3
+
+
+/**
+  This is happening because obj3 inherits obj2 which inherits obj1.
+  
+  when we inherit a value from parent we can only read that value
+  if we try to change it then it becomes part of the main object.
+  
+*/
+
+//=========================
 
 /** given obj1 and obj2, how to find out if obj2 was created from obj1 */
 
